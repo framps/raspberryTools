9742356791e35d479275f999cbfcc795cd0c5e81
framp <framp@linux-tips-and-tricks.de>
2022-11-25 18:58:08 +0100

    Update findRaspis.sh

--- findRaspis.sh
+++ findRaspis.sh
@@ -47,7 +47,7 @@ fi
 # define defaults
 
 DEFAULT_SUBNETMASK="192.168.0.0/24"
-DEFAULT_MAC_REGEX="b8:27:eb|dc:a6:32|e4:5f:01"
+DEFAULT_MAC_REGEX="b8:27:eb|dc:a6:32|e4:5f:01|28:CD:C1"
 # see https://udger.com/resources/mac-address-vendor-detail?name=raspberry_pi_foundation
 INI_FILENAME="~/.${MYNAME}"
 
