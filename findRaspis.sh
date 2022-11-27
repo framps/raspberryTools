2dc21b88e616648f5cc1daaddab2c1b6d773bd26
framp <framp@linux-tips-and-tricks.de>
2022-11-27 12:26:51 +0100

    Config file now can also contain descriptions of mac system

--- findRaspis.sh
+++ findRaspis.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #   Find all existing Raspberries in local subnet
 #
 #   Search for mac addresses used by Raspberries iwhich are defined on 
@@ -23,7 +22,7 @@
 
 set -euo pipefail
 
-VERSION=0.5
+VERSION=0.6
 MYSELF="$(basename "$0")"
 MYNAME=${MYSELF%.*}
 
@@ -49,7 +48,7 @@ fi
 DEFAULT_SUBNETMASK="192.168.0.0/24"
 DEFAULT_MAC_REGEX="b8:27:eb|dc:a6:32|e4:5f:01|28:CD:C1"
 # see https://udger.com/resources/mac-address-vendor-detail?name=raspberry_pi_foundation
-INI_FILENAME="~/.${MYNAME}"
+INI_FILENAME=${HOME}/.${MYNAME}
 
 # help text
 
@@ -67,12 +66,15 @@ Defaults:
 Example:	
 	$MYSELF 192.168.179.0/24
 	
-Init file $INI_FILENAME can be used to customize the mac addresses scanned for. Every line has to define a mac regex
+Init file $INI_FILENAME can be used to customize the mac address scan and descriptions. 
+First optional line can be the regex for the macs to scan. See default above for an example.
+All following lines can contain a mac and a description separated by a space to add a meaningful
+description to the system which owns this mac. Otherwise the hostname discovered will used as the description.
 
-	Example file contents for $INI_FILENAME:	
-b8:27:eb
-dc:a6:32
-e4:5f:01
+	Example file contents for $INI_FILENAME:
+b8:27:eb|dc:a6:32|e4:5f:01
+b8:27:eb:b8:27:eb VPN Server
+b8:27:eb:b8:28:eb Web Server
 	
 EOH
 	exit 0
@@ -84,17 +86,14 @@ MY_NETWORK=${1:-$DEFAULT_SUBNETMASK}
 
 # read property file with mac regexes
 
-if [[ ! -f $INI_FILENAME ]]; then
-	MY_MAC_REGEX="$DEFAULT_MAC_REGEX"
-else
-	echo "Using Mac Regex from $INI_FILENAME"
-	MY_MAC_REGEX=""
-	while read line; do 
-		if [[ -n $MY_MAC_REGEX ]]; then
-			MY_MAC_REGEX="$MY_MAC_REGEX|"
-		fi
-		MY_MAC_REGEX="$MY_MAC_REGEX$line"
-	done < $INI_FILENAME
+MY_MAC_REGEX="$DEFAULT_MAC_REGEX"
+
+if [[ -f "$INI_FILENAME" ]]; then
+	MY_MAC_REGEX_FROM_INI="$(head -n 1 $INI_FILENAME | cut -f 2 -d " ")" 
+	if [[ -z "$MY_MAC_REGEX_FROM_INI" ]]; then
+		echo "Using Mac Regex from $INI_FILENAME"
+		MY_MAC_REGEX="$(head -n 1 $INI_FILENAME)"
+	fi
 fi	
 MY_MAC_REGEX=" (${MY_MAC_REGEX})"
 
@@ -116,21 +115,35 @@ done < <(nmap -sP $MY_NETWORK &>/dev/null; arp -n | grep -E " $MY_MAC_REGEX")
 
 if (( ${#macAddress[@]} > 0 )); then
 
-	printf "\n%-15s %-17s %s\n" "IP address" "Mac address" "Hostname"
+	printf "\n%-15s %-17s %s\n" "IP address" "Mac address" "Hostname/Description"
 
 	for ip in "${!macAddress[@]}"; do
 		set +e
 		h="$(host "$ip")"
 		rc=$?
 		set -e
+		host=""
+		hostMapped=""
 		if (( ! $rc )); then
 			# 12.0.168.192.in-addr.arpa domain name pointer asterix.
 			read arpa dummy dummy dummy host rest <<< "$h"
 			host=${host::-1} # delete trailing "."
-		else
+		fi
+
+		if [[ -f $INI_FILENAME ]]; then
+			set +e
+			hostMapped="$(grep "${macAddress[$ip]}" $INI_FILENAME)"
+			rc=$?
+			set -e
+			if (( ! $rc )); then
+				host="$(cut -f 2- -d ' ' <<< "$hostMapped")"
+			fi
+		fi
+
+		if [[ -z "$host" ]]; then	
 			host="-Unknown-"
 		fi
-		printf "%-15s %17s %s\n" $ip ${macAddress[$ip]} $host
+		printf "%-15s %17s %s\n" $ip ${macAddress[$ip]} "$host"
 	done 
 else
 	echo "No Raspberries found with mac regex $MY_MAC_REGEX"
