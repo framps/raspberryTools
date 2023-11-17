55748126614a4fbfd686c40918b655783831254c
framp <framp@linux-tips-and-tricks.de>
2023-11-17 12:03:53 +0100

    Added rpi hw version detection code from raspi-config

--- raspiKernelInfo.sh
+++ raspiKernelInfo.sh
@@ -20,7 +20,75 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+#	  Note: This code reuses some code from raspi-config (See https://github.com/RPi-Distro/raspi-config/blob/bookworm/raspi-config)
+#
 #######################################################################################################################
+
+is_pi () {
+  ARCH=$(dpkg --print-architecture)
+  if [ "$ARCH" = "armhf" ] || [ "$ARCH" = "arm64" ] ; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# tests for Pi 1, 2 and 0 all test for specific boards...
+
+is_pione() {
+  if grep -q "^Revision\s*:\s*00[0-9a-fA-F][0-9a-fA-F]$" /proc/cpuinfo; then
+    return 0
+  elif grep -q "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]0[0-36][0-9a-fA-F]$" /proc/cpuinfo ; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+is_pitwo() {
+  grep -q "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]04[0-9a-fA-F]$" /proc/cpuinfo
+  return $?
+}
+
+is_pizero() {
+  grep -q "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]0[9cC][0-9a-fA-F]$" /proc/cpuinfo
+  return $?
+}
+
+# ...while tests for Pi 3 and 4 just test processor type, so will also find CM3, CM4, Zero 2 etc.
+
+is_pithree() {
+  grep -q "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F]2[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]$" /proc/cpuinfo
+  return $?
+}
+
+is_pifour() {
+  grep -q "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F]3[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]$" /proc/cpuinfo
+  return $?
+}
+
+is_pifive() {
+  grep -q "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F]4[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]$" /proc/cpuinfo
+  return $?
+}
+
+get_pi_type() {
+  if is_pione; then
+    echo 1
+  elif is_pitwo; then
+    echo 2
+  elif is_pithree; then
+    echo 3
+  elif is_pifour; then
+    echo 4
+  elif is_pifive; then
+    echo 5
+  elif is_pizero; then
+    echo 0
+  else
+    echo -1
+  fi
+}
 
 function displayAndExec() {
 
@@ -32,8 +100,14 @@ function displayAndExec() {
     fi
 }
 
-displayAndExec "uname -a"
-displayAndExec "dpkg --print-architecture"
-displayAndExec "getconf LONG_BIT"
-displayAndExec "echo \$XDG_SESSION_TYPE"
-[[ -f /boot/config.txt ]] && displayAndExec "grep arm_64bit /boot/config.txt"
+if is_pi; then
+	echo "---RPi HW version $(get_pi_type) detected"
+
+	displayAndExec "uname -a"
+	displayAndExec "dpkg --print-architecture"
+	displayAndExec "getconf LONG_BIT"
+	displayAndExec "echo \$XDG_SESSION_TYPE"
+	[[ -f /boot/config.txt ]] && displayAndExec "grep arm_64bit /boot/config.txt"
+else
+	echo "No RaspberryPi detected"
+fi
