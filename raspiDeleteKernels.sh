ec20daa6d5443a0f7aac7493af562ae1f736e461
framp <framp@linux-tips-and-tricks.de>
2023-12-12 18:24:13 +0100

    s

--- raspiDeleteKernels.sh
+++ raspiDeleteKernels.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ls -1 /boot | grep -v -E $(uname -m | sed -E 's/arm(v[0-9]+)l?/\1/') | grep -E "^(config-|initrd|System\.map)" | xargs -0 echo
+# ls -1 /boot | grep -v -E $(uname -r | sed -E 's/arm(v[0-9]+)l?/\1/') | grep -E "^(config-|initrd|System\.map)" | xargs -0 echo
 
 # kernel.img is 32-bit for BCM2835 (RPi1, B+ & Zero)
 # kernel7.img is 32-bit for BCM2836 (RPi2B) and BCM2837 (RPi3iA & RPi3B & RPi3B+)
@@ -33,3 +33,18 @@
 #done
 #
 #sudo apt autoremove
+
+sudo apt-cache search "linux-image-" | grep -v -E "dbg|header|meta" | grep rpi
+
+uname -r
+6.1.0-rpi7-rpi-v8
+sudo apt purge linux-image-6.1.0-rpi6-rpi-2712
+ls /boot # kernel no longer there
+sudo apt install linux-image-6.1.0-rpi6-rpi-2712
+ls /boot # kernel is there again
+
+#linux-images.img-6.1.0-rpi6-rpi-2712
+#linux-images.img-6.1.0-rpi6-rpi-v8
+#linux-images.img-6.1.0-rpi7-rpi-2712
+
+# ls -1 /boot | grep -v -E $(uname -r) | grep -E "^initrd" | sed 's/initrd/linux-image/; s/\.img//' | xargs echo | tee kernels.lst; sudo mv kernels.lst /boot
