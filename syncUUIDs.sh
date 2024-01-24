4288655bd8c0b5692aa451059fd6c73b943d0d3b
framp <framp@linux-tips-and-tricks.de>
2024-01-24 20:20:47 +0100

    Fixed incorrect help text and did some code reformatting

--- syncUUIDs.sh
+++ syncUUIDs.sh
@@ -2,9 +2,12 @@
 #
 #######################################################################################################################
 #
-#   Check and optionally update /boot/cmdline.txt and /etc/fstab on a device with RaspbianOS installed
+#   Check and optionally update /boot/cmdline.txt and /etc/fstab on a device with installed RaspbianOS
 #   with the actual UUIDs/PARTUUIDs of the device. Useful if a cloned RaspbianOS fails to boot because
 #   of UUID/PARTUUID mismatch.
+#
+#   Note: Starting with Bookworm /boot/cmdline.txt is a link to /boot/firmware/cmdline.txt and this file
+#   is actually updated.
 #
 #   Copyright (C) 2022-2024 framp at linux-tips-and-tricks dot de
 #
@@ -35,7 +38,7 @@ readonly MOUNTPOINT="/mnt"
 dryrun=1                # default, enable update with option -u
 verbose=0
 
-mismatchDetected=0      
+mismatchDetected=0
 fstabSaved=0
 
 trap 'umount $MOUNTPOINT &>/dev/null' SIGINT SIGTERM EXIT
@@ -62,10 +65,10 @@ function parseFstab {
 
     mount $rootPartition $MOUNTPOINT 2>/dev/null
 
-   	if [[ ! -e ${MOUNTPOINT}/${FSTAB} ]]; then
+    if [[ ! -e ${MOUNTPOINT}/${FSTAB} ]]; then
         echo "??? Unable to find ${MOUNTPOINT}/${CMDLINE}"
         exit 42
-	fi
+    fi
 
     while read tgt mnt r; do
         case $mnt in
@@ -102,7 +105,9 @@ function updateUUIDinFstab() { # bootType uuid newUUID
         cp ${MOUNTPOINT}/${FSTAB} ${MOUNTPOINT}/${FSTAB}.bak
         fstabSaved=1
     fi
+
     echo "--- Updating $1 $2 to $3 in $rootPartition/$FSTAB"
+
     if ! sed -i --follow-symlinks  "s/^$1=$2/$1=$3/" ${MOUNTPOINT}/${FSTAB}; then
         echo "??? Unable to update $rootPartition/$FSTAB"
         exit 1
@@ -111,14 +116,19 @@ function updateUUIDinFstab() { # bootType uuid newUUID
 }
 
 function updateUUIDinCmdline() { # bootType uuid newUUID
+
     if (( $dryrun )); then
         echo "!!! $1 $2 should be updated to $3 in $bootPartition/$CMDLINE"
         return
     fi
+
     echo "--- Creating cmdline backup ${CMDLINE}.bak on $bootPartition"
+
     mount $bootPartition $MOUNTPOINT 2>/dev/null
     cp ${MOUNTPOINT}/${CMDLINE} ${MOUNTPOINT}/${CMDLINE}.bak
+
     echo "--- Updating $1 $2 to $3 in $bootPartition/cmdline.txt"
+
     if ! sed -i  --follow-symlinks "s/$1=$2/$1=$3/" ${MOUNTPOINT}/${CMDLINE}; then
         echo "??? Unable to update $bootPartition/$CMDLINE"
         exit 1
@@ -127,22 +137,22 @@ function updateUUIDinCmdline() { # bootType uuid newUUID
 }
 
 function usage() {
+
 	cat <<- EOF
 	$MYSELF - $VERSION
-    
+
     Synchronize UUIDs or PARTUUIDs in /etc/fstab and /boot/cmdline.txt
     with existing UUIDs or PARTUUIDs of device partitions.
     If no option is passed the used UUIDs or PARTUUIDs are retrieved
     and displayed only. No files are updated.
-    
+
     Usage: $0 [-uv] device
     -u: Create backup of files and update the UUIDs and PARTUUIDs in
-        /etc/cmdline.txt and /etc/fstab
+        /boot/cmdline.txt and /etc/fstab
     -v: Verbose output"
-    
+
     Device examples: /dev/sda, /dev/mmcblk0, /dev/nvme0n1
 EOF
-    exit 0
 }
 
 #
@@ -186,10 +196,12 @@ case $device in
         bootPartition="${device}1"
         rootPartition="${device}2"
         ;;
+
     /dev/mmcblk*|/dev/nvme*)
         bootPartition="${device}p1"
         rootPartition="${device}p2"
         ;;
+
     *)
         echo "Invalid device $device"
         echo "Device examples: /dev/sda, /dev/mmcblk0, /dev/nvme0n1"
@@ -254,7 +266,7 @@ if [[ -z $actualCmdlineRootUUID || \
 	]]; then
 		echo "??? ASSERTION FAILED: Unable to collect required data"
 	exit 1
-fi	
+fi
 
 if (( $verbose )); then
 	echo "--- $cmdlineRootType $cmdlineRootUUID used in $bootPartition/cmdline"
@@ -269,15 +281,15 @@ if [[ $cmdlineRootUUID == $actualCmdlineRootUUID ]]; then
 else
 	updateUUIDinCmdline $cmdlineRootType $cmdlineRootUUID $actualCmdlineRootUUID
     mismatchDetected=1
-fi	
+fi
 
 if [[ $fstabBootUUID == $actualFstabBootUUID ]]; then
     echo "--- Boot $fstabBootType $actualFstabBootUUID already used in $rootPartition/$FSTAB"
 else
 	updateUUIDinFstab $fstabBootType $fstabBootUUID $actualFstabBootUUID
     mismatchDetected=1
-fi	
-    
+fi
+
 if [[ $fstabRootUUID == $actualFstabRootUUID ]]; then
     echo "--- Root $fstabRootType $actualFstabRootUUID already used in $rootPartition/$FSTAB"
 else
