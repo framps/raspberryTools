9f340fca3f2d10a3626b9b07a5777c47e7f64f3f
framp <framp@linux-tips-and-tricks.de>
2025-11-14 21:15:20 +0100

    Added support for CM4

--- monitorCPUTemperature.sh
+++ monitorCPUTemperature.sh
@@ -2,9 +2,10 @@
 
 #######################################################################################################################
 #
-#    Monitor CPU temperature and fan speed of a RPi5 with an original active cooler
-#	 First parameter defines the monitor interval. Default is 3 seconds.
-#	 All measurements are recorded in a log file
+#    Monitor CPU temperature and CPU frequency of a Raspberry. If there exists an original active cooler
+#    the fan speed will also be reported.
+#    First parameter defines the monitor interval. Default is 3 seconds.
+#    All measurements are recorded in a log file
 #
 #######################################################################################################################
 #
@@ -30,7 +31,7 @@
 readonly MYSELF="$(basename "$0")"
 readonly MYNAME=${MYSELF##*/}
 readonly LOGFILE="${MYNAME/.sh/}.log"
-readonly VERSION="0.1"
+readonly VERSION="0.2"
 readonly GITREPO="https://github.com/framps/raspberryTools"
 
 DELAY=${1:-3} # first parm defines the delay if specified
@@ -42,10 +43,8 @@ if ! $which vcgencmd &> /dev/null; then
     exit 42
 fi
 
-if [[ -z $(ls /sys/devices/platform/cooling_fan/hwmon/*/fan1_input) ]]; then
-    echo "No active cooler detected."
-    exit 42
-fi
+[[ -d /sys/devices/platform/cooling_fan ]]
+FAN_AVAILABLE=$((! $? ))
 
 cnt=0
 
@@ -66,9 +65,14 @@ function finish() {
 
 trap 'finish $?' SIGINT
 
-echo -e "Time \t\tFan \tCPU \tPMIC \tFreq" | tee -a "$LOGFILE"
+if (( $FAN_AVAILABLE )); then
+	echo -e "Time \t\tFan \tCPU \tPMIC \tFreq" | tee -a "$LOGFILE"
+else
+	echo -e "Time \t\tCPU \tPMIC \tFreq" | tee -a "$LOGFILE"
+fi
+
 while true; do
-    FAN="$(< /sys/devices/platform/cooling_fan/hwmon/*/fan1_input)"
+    (( $FAN_AVAILABLE )) &&  FAN="$(< /sys/devices/platform/cooling_fan/hwmon/*/fan1_input)"
     CPU="$(vcgencmd measure_temp | cut -d "=" -f 2)"
     CPU="${CPU//\'C/}"
     PMIC="$(vcgencmd measure_temp pmic | cut -d "=" -f 2)"
@@ -76,7 +80,12 @@ while true; do
     FREQ="$(vcgencmd measure_clock arm | cut -d "=" -f 2)"
     FREQ="$(($FREQ / 1000000))"
     TIME="$(date +%H:%M:%S)"
+if (( $FAN_AVAILABLE )); then
     echo -e "$TIME \t$FAN \t$CPU \t$PMIC\t$FREQ" | tee -a "$LOGFILE"
+else
+  echo -e "$TIME \t$CPU \t$PMIC\t$FREQ" | tee -a "$LOGFILE"
+fi
     ((cnt++))
     sleep $DELAY
 done
+
